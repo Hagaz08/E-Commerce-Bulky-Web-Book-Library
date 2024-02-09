dc8bb1c11c0b07d8290457b126a44b7f7ec8b252
Hagaz <Taiwo.sadiq@decagon.dev>
2024-02-09 10:54:04 +0100

    Add project files.

---
+++ BulkyWeb_Sadiq/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyWeb_Sadiq.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
